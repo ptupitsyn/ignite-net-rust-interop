54502fa4569b9d01ad1d37b6567548f15d01d7ad
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-17 15:13:09 +0200

    Add libignite code

---
+++ dotnet/libignite/Exports.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using Apache.Ignite.Core.Log;
+
+public static class Exports
+{
+    private static readonly Lazy<IIgniteClient> Client = new(
+        () => Ignition.StartClient(new IgniteClientConfiguration("127.0.0.1:10800")
+        {
+            Logger = new ConsoleLogger
+            {
+                MinLevel = LogLevel.Trace
+            }
+        }));
+
+    [UnmanagedCallersOnly(EntryPoint = "CachePut")]
+    public static void CachePut(int key, int val) => GetOrCreateCache().Put(key, val);
+
+    [UnmanagedCallersOnly(EntryPoint = "CacheGet")]
+    public static int CacheGet(int key) => GetOrCreateCache().Get(key);
+
+    private static ICacheClient<int, int> GetOrCreateCache() =>
+        Client.Value.GetOrCreateCache<int, int>("c");
+}
